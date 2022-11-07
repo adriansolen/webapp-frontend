0e1d0ebbffb3dda6ec4b78c335182a92e3b96a7a
adriansolen <adrianmsolen@gmail.com>
2022-11-08 02:41:27 +0700

    Changed Values of Homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -11,7 +11,7 @@
                 <!-- Masthead Avatar Image-->
                 <img class="masthead-avatar mb-5" src="assets-homepage/assets/img/avataaars.svg" alt="..." />
                 <!-- Masthead Heading-->
-                <h1 class="masthead-heading text-uppercase mb-0">Start Bootstrap</h1>
+                <h1 class="masthead-heading text-uppercase mb-0">Adrian Solen</h1>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
@@ -19,7 +19,7 @@
                     <div class="divider-custom-line"></div>
                 </div>
                 <!-- Masthead Subheading-->
-                <p class="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
+                <p class="masthead-subheading font-weight-light mb-0">Athlete - Gamer - Student</p>
             </div>
         </header>
         <!-- Portfolio Section-->
